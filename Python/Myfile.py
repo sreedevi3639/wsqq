bf94eebc4c51b95ffd7c2f42c0e2ae3d2d4222eb
sreedevi3639 <37687597+sreedevi3639@users.noreply.github.com>
2020-02-07 00:52:35 +0530

    vvv
    ffsf

---
+++ Python/Myfile.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+if 5 > 2:
+    print("Five is greater than two!!!!!!!!!!!!")
+
+("Five is greater than two!")
+if 5 > 2:
+if 5 > 2:
+
+        print("Five is greater than two!") 
+x = "HI"
+x = 5
+print(x)
+#print(y)
+# To make debugging of logistic_regression module easier we enable imported modules autoreloading feature.
+# By doing this you may change the code of logistic_regression library and all these changes will be available here.
+%load_ext autoreload
+%autoreload 2
+
+# Add project root folder to module loading paths.
+import sys
+sys.path.append('../..')
+
+"""x = "awesome"
+
+def myfunc():
+    x = "Fentastic"
+    print("python is " + x)
+    print(type(x))
+
+myfunc()
+
+print("python is " + x)
+print(type(x))
+
+x = 1j
+print(type(x))
+
+import random
+print (random.randrange(1,10))
+
+x = str("s1")
+y = float(2)
+z = str(3.0)
+print(x)
+print(y)
+print(type(y))
+print(type(z))
+x = 12
+
+x |= 3
+
+print(x)
+thislist = ["abc","bcf",'abc']
+print (type(thislist))
+print(thislist[-1])
+
+thistuple = ("abc","bcf",'abc')
+for x in thistuple:
+ print(x)
+ print(len(thistuple))
+ thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+del thisset()
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+def my_func(fname):
+    print(fname + " Hiii ")
+
+my_func("email")
+my_func("sub")
+my_func("Libus")
+"""
+
+
+# In[ ]:
+
+x = lambda a: a * 10
+print (x(10))
+
+
+
+
+
+
+
+
+
+
+
+
